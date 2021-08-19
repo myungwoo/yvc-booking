c2dcf458466ad15f28de0db52b70c097acf3971b
Myungwoo Chun <mc.tamaki@gmail.com>
2021-08-19 21:43:44 +0900

    웹소켓 중개 때문에 instance 수를 1개로 고정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
   apps : [{
     name: 'yvcbooking',
     script: 'server.js',
-    instances: 0,
+    instances: 1,
     exec_mode: 'cluster',
     wait_ready: true,
     listen_timeout: 50000,
